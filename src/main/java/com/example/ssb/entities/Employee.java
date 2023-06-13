811c2f27b01ee3d11247b2ebc71505ab06d0da77
ziulluiz <luis.guzp13@gmail.com>
2023-06-13 11:51:37 -0600

    employee and suppliers entities created

---
+++ src/main/java/com/example/ssb/entities/Employee.java
@@ -0,0 +1,24 @@
+package com.example.ssb.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private Double salary;
+}
---
+++ src/main/java/com/example/ssb/entities/Supplier.java
@@ -0,0 +1,24 @@
+package com.example.ssb.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "suppliers")
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String address;
+}
